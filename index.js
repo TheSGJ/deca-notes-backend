a001866c9874fa1d990725e0ded0573116137ab6
Shri Ganesh Jha <60005430+TheSGJ@users.noreply.github.com>
2023-02-09 22:34:12 +0000

    cors added

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const connectToMongo = require('./db');
 const express = require('express');
 
 connectToMongo();
-
+var cors = require('cors')
 const app = express()
 const port = 5000
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
