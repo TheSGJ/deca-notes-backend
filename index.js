26cf64c8ec18c5bee7820cb413cd053edeb874b6
Shri Ganesh Jha <realsgj@gmail.com>
2023-02-19 10:37:47 +0000

    new cors code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const connectToMongo = require('./db');
 const express = require('express');
 
@@ -8,9 +9,18 @@ const app = express()
 const port = 5000
 
 app.use(express.json())
-app.use(cors())
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+var whitelist = [process.env.CORS_ALLOWED_HOST, process.env.CORS_ALLOWED_HOST_2]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use('/api/auth', cors(corsOptions), require('./routes/auth'))
+app.use('/api/notes', cors(corsOptions), require('./routes/notes'))
 app.listen(port, () => {
   console.log(`Deca notes backend app listening on port ${port}`)
 })
